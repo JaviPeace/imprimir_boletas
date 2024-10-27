11f559dae0e85f78c695eb320c6eae12edeca463
JaviPeace <javiera.azcar@uc.cl>
2024-10-27 00:45:39 -0300

    arreglo formato boleta

--- boletas-pdf/generateBoleta.js
+++ boletas-pdf/generateBoleta.js
@@ -1,31 +1,35 @@
 const AWS = require('aws-sdk');
-const { PDFDocument } = require('pdf-lib');
+const { PDFDocument, rgb } = require('pdf-lib');
 
 const s3 = new AWS.S3();
 
 module.exports.handler = async (event) => {
   const data = JSON.parse(event.body);
-  const { grupo, usuario, partido, valorTotal } = data;  
+  const { liga, usuario, partido, valorTotal } = data;  
 
-  const { nombre: nombreUsuario, email } = usuario;
+  const { nombre: nombreUsuario } = usuario;
   const { equipo1, equipo2, fecha, ubicacion } = partido;
 
   // Crear el PDF
   const pdfDoc = await PDFDocument.create();
   const page = pdfDoc.addPage([500, 700]);
 
-  page.drawText(`Grupo: ${grupo}`, { x: 50, y: 650, size: 18 });
-  page.drawText(`Usuario: ${nombreUsuario} (${email})`, { x: 50, y: 620, size: 15 });
-  page.drawText(`Partido: ${equipo1} vs ${equipo2}`, { x: 50, y: 590, size: 15 });
-  page.drawText(`Fecha: ${fecha}`, { x: 50, y: 560, size: 15 });
-  page.drawText(`Ubicación: ${ubicacion}`, { x: 50, y: 530, size: 15 });
-  
-  page.drawText(`Valor Total: $${valorTotal.toFixed(2)}`, { x: 50, y: 500, size: 15 }); 
+ // Agregar título
+page.drawText('Boleta emitida por el grupo 12', { x: 50, y: 680, size: 20, color: rgb(0.1176, 0.5098, 0.7843) }); 
+
+// Resto del contenido
+page.drawText(`Liga: ${liga}`, { x: 50, y: 650, size: 15 });
+page.drawText(`Usuario: ${nombreUsuario}`, { x: 50, y: 620, size: 15 });
+page.drawText(`Partido: ${equipo1} vs ${equipo2}`, { x: 50, y: 590, size: 15 });
+page.drawText(`Fecha: ${fecha}`, { x: 50, y: 560, size: 15 });
+page.drawText(`Ubicación: ${ubicacion}`, { x: 50, y: 530, size: 15 });
+page.drawText(`Valor Total: $${valorTotal.toFixed(2)}`, { x: 50, y: 500, size: 15 });
+
 
   const pdfBytes = await pdfDoc.save();
 
   const bucketName = 'imprimir-boletas';
-  const fileName = `boletas/${grupo}/${nombreUsuario}-${equipo1}-vs-${equipo2}.pdf`;
+  const fileName = `boletas/${liga}/${nombreUsuario}-${equipo1}-vs-${equipo2}.pdf`;
 
   try {
     await s3.putObject({
